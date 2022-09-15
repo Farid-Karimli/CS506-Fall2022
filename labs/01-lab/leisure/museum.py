b0a804edf6998557226a788d02c135a7d9bc59c8
Farid Karimli <karimli.farid@icloud.com>
2022-09-15 18:33:30 -0400

    Changes made

--- labs/01-lab/leisure/museum.py
+++ labs/01-lab/leisure/museum.py
@@ -1,3 +1,13 @@
 def draw_museum():
-    print("museum not found")
+    
+    print(" ==================")
+    for x in range(10):
+        print("||", end="")
+        if x==0:
+            print(" MUSEUM OF ARTS ", end="")
+        else:
+            print("                ", end="")
+        print("||")
+        
     return
+draw_museum()
